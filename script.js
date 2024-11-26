623ea88362d25ecb9d3c35a7d480e7d8577e87fe
Ali <alouloucheikh05@gmail.com>
2024-11-26 22:11:16 +0100

    "Updated index.html with new layout and design ✨, added form elements and JavaScript code for image upload and dynamic social links, and modified script.js with commented out code for mobile menu toggle."

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
 //🏗️under construction🚧
+// const mobileMenu = document.getElementById('mobile-menu');
+// const mobileMenuToggle = document.getElementById('mobile-menu-toggle');
+// const mobileMenuClose = document.getElementById('mobile-menu-close');
+
+// mobileMenuToggle.addEventListener('click', () => {
+//     mobileMenu.classList.remove('hidden');
+// });
+
+// mobileMenuClose.addEventListener('click', () => {
+//     mobileMenu.classList.add('hidden');
+// });
