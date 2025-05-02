f704902370a8e1a46c02225a3a328c6c5d762f31
Ali <alouloucheikh05@gmail.com>
2025-05-02 18:01:19 +0100

    Update googleScriptUrl in config to new endpoint

--- plans/basic/config.js
+++ plans/basic/config.js
@@ -1,7 +1,7 @@
 export const CONFIG = {
     maxFileSize: 5 * 1024 * 1024,
     allowedTypes: ['image/jpeg', 'image/png', 'image/gif'],
-    googleScriptUrl: 'https://script.google.com/macros/s/AKfycbyfDyJYuheWcQ7oX-rPUg762oCprXFxzK0Jo6-qQv9a9qcM7ZfFqsS6APBrK1DyFvc8/exec', // date 2025-5-2 time 11:04am
+    googleScriptUrl: 'https://script.google.com/macros/s/AKfycbzXkyhl6-jGudfQtUVFXNYTymW7NygihNdEGGf0Xegve9R7fJbvkX5ZUlNDZWjaOnXZ/exec', // date 2025-5-2 time 11:04am
     emailRegex: /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/,
     linkRegex: /^[a-zA-Z][a-zA-Z0-9-]{2,14}$/, // 3-15 chars total: 1 letter + 2-14 more chars
 };
