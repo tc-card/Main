aac6759ce3719e4daab3c4ec27471d0f64f13412
Ali <alouloucheikh05@gmail.com>
2025-06-18 23:11:39 +0100

    fix: Update googleScriptUrl to the latest version for improved functionality

--- plans/basic/config.js
+++ plans/basic/config.js
@@ -1,7 +1,7 @@
 export const CONFIG = {
     maxFileSize: 5 * 1024 * 1024,
     allowedTypes: ['image/jpeg', 'image/png', 'image/gif'],
-    googleScriptUrl: 'https://script.google.com/macros/s/AKfycbzXkyhl6-jGudfQtUVFXNYTymW7NygihNdEGGf0Xegve9R7fJbvkX5ZUlNDZWjaOnXZ/exec', // date 2025-5-2 time 11:04am
+    googleScriptUrl: 'https://script.google.com/macros/s/AKfycbzWi4ojXKyobu7r43puIjMHVpy44aL27Z-A-gSWMn_ka3K1hr3B_sxSQ0K6vsvJi-d_/exec', // date 2025-6-18 time 11:09pm
     emailRegex: /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/,
     linkRegex: /^[a-zA-Z][a-zA-Z0-9-]{2,14}$/, // 3-15 chars total: 1 letter + 2-14 more chars
 };
